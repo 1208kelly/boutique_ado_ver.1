af6282ee835b7b78b9cbeaffb18d176aefd430fb
1208kelly <1208kelly@gmail.com>
2020-08-11 10:12:10 +0000

    Added checkout views and template

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from.models import Order
+from .models import Order
 
 
-class OrderForm(forms.ModelForms):
+class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
